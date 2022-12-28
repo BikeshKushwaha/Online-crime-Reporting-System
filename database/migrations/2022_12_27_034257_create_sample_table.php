b36c24e70e40ee8565e6647be80edc7eef0affe5
SandhyaSah001 <121339263+SandhyaSah001@users.noreply.github.com>
2022-12-28 10:04:04 +0545

    new table added

--- database/migrations/2022_12_27_034257_create_sample_table.php
+++ database/migrations/2022_12_27_034257_create_sample_table.php
@@ -17,6 +17,7 @@ class CreateSampleTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('gdfgfgdf');
+            $table->string('address');
             $table->timestamps();
         });
     }
